712826d910a18ad265d19feb1f94b1e40915f10d
amanda_laughlin <a.c.laughlin@gmail.com>
2023-03-13 11:11:27 -0600

    Trying to update popular posts widget in css

--- content.php
+++ content.php
@@ -20,13 +20,16 @@
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</header>
 		<div class="entry-summary">
+		<?php if ( has_post_thumbnail() ) : ?>
+  <figure><?php the_post_thumbnail('full'); ?></figure>
+<?php endif; ?>
 			<?php the_content(); ?>
 		</div>
 		<footer class="entry-footer">
 			<span class="entry-terms comments author">
 				Written by <?php the_author(); ?> /
 				Posted in <?php the_category(', '); ?> /
-				
+
 				<a href="<?php comments_link(); ?>"><?php comments_number('<span>No Comments</span>', '1 Comment', '% comments'); ?></a>
 			</span>
 		</footer>
